58b400a8b0543bfd96d8227d7791ea0fa00b87d1
Isaac Escudero <isaac_escudero@euneiz.com>
2026-04-26 23:53:21 +0200

    Se ha comentado el AudioManager

--- JuegoARIsaacEscudero/Assets/IsaacEscudero/Scripts/AudioManager.cs
+++ JuegoARIsaacEscudero/Assets/IsaacEscudero/Scripts/AudioManager.cs
@@ -3,11 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
+    //Audio Sources donde saldran los clips
     [SerializeField] AudioSource audioSourceBackgroundMusic;
     [SerializeField] AudioSource audioSourceSFX;
+    //Los clips a reproducir
     [SerializeField] AudioClip audioClipBackgroundMusic;
     [SerializeField] AudioClip audioClipPickSound;
+    //Variable para acceder a la UI de preparacion de juego
     UIForPreparingJuegoScene uIPrep;
+    //Variable usada para singelton
     public static AudioManager instance;
     private void Awake()
     {
@@ -20,12 +24,16 @@ public class AudioManager : MonoBehaviour
             Destroy(this);
         }
         uIPrep = GetComponentInParent<UIForPreparingJuegoScene>();
+        //Si no estamos en la escena de juego y la UI de preparacion de juego esta desactivada
+        // o si  estamos en la escena de juego y la UI de preparacion de juego esta activada
         if (SceneManager.GetActiveScene().buildIndex != 1 && !uIPrep.GetCanvasPrepararJuego().enabled
             || SceneManager.GetActiveScene().buildIndex == 1 && uIPrep.GetCanvasPrepararJuego().enabled)
         {
+            //Se desactiva este script
             this.enabled = false;
         }
     }
+    //Funciones para reproducir musica y efectos de sonido
     internal void PlayBackgroundMusic()
     {
         audioSourceBackgroundMusic.PlayOneShot(audioClipBackgroundMusic);
@@ -34,6 +42,7 @@ public class AudioManager : MonoBehaviour
     {
         audioSourceSFX.PlayOneShot(audioClipPickSound);
     }
+    //Funcion para parar todos los sonidos
     internal void StopAllAudioSources()
     {
         audioSourceBackgroundMusic.Stop();
