abc0207bc48597e7715c21048c9071d1c4cfb40b
Isaac Escudero <isaac_escudero@euneiz.com>
2026-04-27 13:51:49 +0200

    Se ha comentado y limpiado el codigo

--- JuegoARIsaacEscudero/Assets/IsaacEscudero/Scripts/UIForPreparingJuegoScene.cs
+++ JuegoARIsaacEscudero/Assets/IsaacEscudero/Scripts/UIForPreparingJuegoScene.cs
@@ -6,31 +6,47 @@ using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 public class UIForPreparingJuegoScene : MonoBehaviour
 {
+    #region Atributos
+    //Referencia al manager de planos
     ARPlaneManager aRPlaneManager;
+    //Referencia al menu de configuracion
     ConfigurationMenu configurationMenu;
+    //Referencia a la UI del juego
     UIJuegoScene juegoGemas;
+    //Referencia al script de generar las gemas
     GenerarGemas generarGemas;
+    //Textos para cada tipo de plano
     [SerializeField] TMP_Text textForHorizontalPlanesDetected;
     [SerializeField] TMP_Text textForVerticalPlanesDetected;
+    //Referencia a este canvas
     [SerializeField] Canvas canvas;
+    //Boton para poder iniciar partida
     [SerializeField] Button botonParaJugar;
+    //Variables para contar cuantos planos de cada tipo se han añadido en tiempo real
     int horizontalPlanes;
     int verticalPlanes;
+    //Variables donde se guardaran los valores que se guardaron en el menu de configuracion
     float horizontalPlanesMin;
     float verticalPlanesMin;
     float tiempoJuego;
     bool estaLaOclusionActivada;
+    //Referencia al Manager de la oclusion de imagenes AR
     [SerializeField] AROcclusionManager aROcclusionManager;
+    #endregion
+    #region Metodos de Unity
     private void Awake()
     {
+        //Conseguimos los componentes
         aRPlaneManager = GetComponent<ARPlaneManager>();
         configurationMenu = GetComponentInChildren<ConfigurationMenu>();
         juegoGemas = GetComponent<UIJuegoScene>();
         generarGemas = GetComponentInChildren<GenerarGemas>();
+        //Conseguimos los parametros de la escena del menu
         horizontalPlanesMin = configurationMenu.GetPlanosHorizontalesMaximos();
         verticalPlanesMin = configurationMenu.GetPlanosVerticalesMaximos();
         tiempoJuego = configurationMenu.GetTiempo();
         estaLaOclusionActivada = configurationMenu.GetOclusion();
+        //Desactivamos o activamos la oclusion de imagenes
         if (estaLaOclusionActivada)
         {
             aROcclusionManager.enabled = true;
@@ -39,7 +55,9 @@ public class UIForPreparingJuegoScene : MonoBehaviour
         {
             aROcclusionManager.enabled = false;
         }
+        //Desactivamos el boton de jugar al inicio
         botonParaJugar.gameObject.SetActive(false);
+        //Comprobamos que estemos en la escena de juego para activar o desactivar este canvas y el manager de planos AR
         if (SceneManager.GetActiveScene().buildIndex != 1)
         {
             canvas.enabled = false;
@@ -51,46 +69,66 @@ public class UIForPreparingJuegoScene : MonoBehaviour
             aRPlaneManager.enabled = true;
         }
     }
+    #endregion
+    #region Metodos
+    //Usamos esta funcion en el TrackablesChanged(ARTrackablesChangedEventArgs<ARPlane> arPlanes) para detectar cuando se cambian los planos
     public void PlanesDetected(ARTrackablesChangedEventArgs<ARPlane> arPlanes)
     {
+        //Se reinician los planos horizontales y verticales para reiniciar los contadores
         horizontalPlanes = 0;
         verticalPlanes = 0;
+        //Se comprueba por cada plano de la lista de trackables del manager de planos AR
         foreach (ARPlane plane in aRPlaneManager.trackables)
         {
-            //Solo contamos los que estan siendo rastreados y son horizontales
+            //Solo contamos los planos que estan siendo rastreados y son horizontales
             if (plane.trackingState != TrackingState.None &&
-               (plane.alignment == PlaneAlignment.HorizontalUp))
+               (plane.alignment == PlaneAlignment.HorizontalUp) || plane.alignment == PlaneAlignment.HorizontalDown)
             {
                 horizontalPlanes++;
             }
+            //Solo contamos los planos que estan siendo rastreados y son verticales
             if (plane.trackingState != TrackingState.None &&
                (plane.alignment == PlaneAlignment.Vertical))
             {
                 verticalPlanes++;
             }
         }
+        //Se muestran por pantalla el numero de planos detectados y el numero de planos necesarios para cada tipo
         textForHorizontalPlanesDetected.text = "Planos horizontales: " + " " + horizontalPlanes + " / " + horizontalPlanesMin;
         textForVerticalPlanesDetected.text = "Planos verticales: " + " " + verticalPlanes + " / " + verticalPlanesMin;
+        //Se activa el boton de jugar si se superan o igualan el numero de planos detectados y el numero de planos necesarios,
+        //tanto horizontales como verticales
         if (horizontalPlanes >= horizontalPlanesMin && verticalPlanes >= verticalPlanesMin)
         {
             botonParaJugar.gameObject.SetActive(true);
         }
+        //Se desactiva si no se cumple alguna de las condiciones anteriores
         else if (horizontalPlanes < horizontalPlanesMin || verticalPlanes < verticalPlanesMin)
         {
             botonParaJugar.gameObject.SetActive(false);
         }
     }
+    //Funcion usada para empezar el juego
     public void Jugar()
     {
+        //Se desactiva este canvas
         canvas.enabled = false;
+        //Se activa el UI del juego
         juegoGemas.GetJuegoGemasCanvas().enabled = true;
+        //Se establecen todos los parametros para empezar a jugar
         juegoGemas.SetParametrosEnJuegoGemas(horizontalPlanesMin, verticalPlanesMin, tiempoJuego);
+        //Se generan las gemas
         generarGemas.SetGemasInScene();
+        //Se activa el audio manager y se reproduce la musica de fondo
         AudioManager.instance.enabled = true;
         AudioManager.instance.PlayBackgroundMusic();
     }
+    #endregion
+    #region Getters
+    //Se usa para conseguir este canvas
     internal Canvas GetCanvasPrepararJuego()
     {
         return canvas;
     }
+    #endregion
 }
--- JuegoARIsaacEscudero/Assets/IsaacEscudero/Scripts/UIJuegoScene.cs
+++ JuegoARIsaacEscudero/Assets/IsaacEscudero/Scripts/UIJuegoScene.cs
@@ -7,20 +7,34 @@ using UnityEngine.XR.ARFoundation;
 
 public class UIJuegoScene : MonoBehaviour
 {
+    #region Atributos
+    //Referencia a este canvas
     [SerializeField] Canvas canvasJuego;
+    //Referencia a la UI de preparar el juego
     UIForPreparingJuegoScene uIPrep;
+    //Textos para mostrar en pantalla
     [SerializeField] TMP_Text textoTiempo;
     [SerializeField] TMP_Text textoGemas;
     [SerializeField] TMP_Text textoFinalDelJuego;
+    //Numero de gemas que se han pillado
     float gemasPilladas = 0;
+    //Numero de gemas totales a recoger
     float gemasTotales;
+    //Variable usada para singelton
     public static UIJuegoScene instance;
+    //Variable para activar y desactivar el cronometro
     bool chronometerActive = false;
+    //Variable para guardar el tiempo
     float tiempo;
+    //Boton para volver al menu inicial
     [SerializeField] Button botonVolverAlMenu;
+    //Indice de la escena del menu de inicio
     [SerializeField] int indiceMenu;
+    #endregion
+    #region Funciones de Unity
     private void Awake()
     {
+        //Singelton
         if (instance == null)
         {
             instance = this;
@@ -30,10 +44,12 @@ public class UIJuegoScene : MonoBehaviour
             Destroy(this);
         }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        //Se consigue este componente
         uIPrep = GetComponent<UIForPreparingJuegoScene>();
+        //Se comprueba que este canvas solo se active en la escena de juego,
+        //cuando se han detectado los planos necesarios para poder jugar y le des al boton de jugar 
         if (SceneManager.GetActiveScene().buildIndex != 1 && !uIPrep.GetCanvasPrepararJuego().enabled
             || SceneManager.GetActiveScene().buildIndex == 1 && uIPrep.GetCanvasPrepararJuego().enabled)
         {
@@ -44,64 +60,95 @@ public class UIJuegoScene : MonoBehaviour
             canvasJuego.enabled = true;
         }
     }
+    
+    private void Update()
+    {
+        //Comprobamos si se ha activado el cronometro o no
+        if (chronometerActive)
+        {
+            //Si el numero de gemas pilladas es mayor o igual al numero de gemas totales
+            if (gemasPilladas >= gemasTotales)
+            {
+                //Se desactiva el cronometro y se muestran el texto de victoria y el boton para regresar al menu principal
+                chronometerActive = false;
+                textoFinalDelJuego.text = "Felicidades, ganaste la partida jugador";
+                botonVolverAlMenu.gameObject.SetActive(true);
+            }
+            //Si el tiempo es mayor o igual a 0
+            else if (tiempo >= 0)
+            {
+                //Se resta el tiempo
+                tiempo -= Time.deltaTime;
+                //Y se actualiza el cronometro en pantalla
+                float minutes = Mathf.FloorToInt(tiempo / 60);
+                float seconds = Mathf.FloorToInt(tiempo % 60);
+                textoTiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            }
+            //Si el tiempo es menor a 0
+            else if (tiempo < 0)
+            {
+                //El tiempo se establece en 0
+                tiempo = 0;
+                //Se actualiza el cronometro en pantalla
+                float minutes = Mathf.FloorToInt(tiempo / 60);
+                float seconds = Mathf.FloorToInt(tiempo % 60);
+                textoTiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+                //Se desactiva el cronometro
+                chronometerActive = false;
+                //Se muestra el texto de haber perdido
+                textoFinalDelJuego.text = "Mala suerte jugador, perdiste";
+                //Se activa el boton para regresar al menu
+                botonVolverAlMenu.gameObject.SetActive(true);
+            }
+        }
+    }
+    #endregion
+    #region Metodos
+    //Funcion para regresar al menu de inicio
+    public void RegresarAlMenu()
+    {
+        //Se detienen todos los audioSources
+        AudioManager.instance.StopAllAudioSources();
+        //Y se carga la escena del menu de inicio
+        LoaderUtility.Deinitialize();
+        LoaderUtility.Initialize();
+        SceneManager.LoadScene(indiceMenu);
+    }
+    //Funcion para actualizar las gemas pilladas
+    internal void ActualizarGemasPilladas()
+    {
+        //Se suma la gema que se ha pillado
+        gemasPilladas++;
+        //Se actualiza el texto de las gemas
+        textoGemas.text = gemasPilladas.ToString() + " / " + gemasTotales.ToString();
+    }
+    #endregion
+    #region Getters y Setters
+    //Se setean los parametros para el juego de las gemas
     internal void SetParametrosEnJuegoGemas(float gemasHorizontales, float gemasVerticales, float tiempo)
     {
+        //Se guarda el tiempo y se actualiza por pantalla
         this.tiempo = tiempo;
         float minutes = Mathf.FloorToInt(tiempo / 60);
         float seconds = Mathf.FloorToInt(tiempo % 60);
         gemasTotales = gemasHorizontales + gemasVerticales;
         textoTiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        //Se actualiza por pantalla el texto de las gemas
         textoGemas.text = gemasPilladas.ToString() + " / " + gemasTotales.ToString();
+        //Se activa el cronometro
         chronometerActive = true;
+        //Se desactiva el boton de regresar al menu de inicio
         botonVolverAlMenu.gameObject.SetActive(false);
     }
-    private void Update()
-    {
-        if (chronometerActive)
-        {
-            if (gemasPilladas >= gemasTotales)
-            {
-                chronometerActive = false;
-                textoFinalDelJuego.text = "Felicidades, ganaste la partida jugador";
-                botonVolverAlMenu.gameObject.SetActive(true);
-            }
-            else if (tiempo >= 0)
-            {
-                tiempo -= Time.deltaTime;
-                float minutes = Mathf.FloorToInt(tiempo / 60);
-                float seconds = Mathf.FloorToInt(tiempo % 60);
-                textoTiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-            }
-            else if (tiempo < 0)
-            {
-                tiempo = 0;
-                float minutes = Mathf.FloorToInt(tiempo / 60);
-                float seconds = Mathf.FloorToInt(tiempo % 60);
-                textoTiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-                chronometerActive = false;
-                textoFinalDelJuego.text = "Mala suerte jugador, perdiste";
-                botonVolverAlMenu.gameObject.SetActive(true);
-            }
-        }
-    }
-    public void RegresarAlMenu()
-    {
-        AudioManager.instance.StopAllAudioSources();
-        LoaderUtility.Deinitialize();
-        LoaderUtility.Initialize();
-        SceneManager.LoadScene(indiceMenu);
-    }
+    //Se consigue este canvas
     internal Canvas GetJuegoGemasCanvas()
     {
         return canvasJuego;
     }
-    internal void ActualizarGemasPilladas()
-    {
-        gemasPilladas++;
-        textoGemas.text = gemasPilladas.ToString() + " / " + gemasTotales.ToString();
-    }
+    //Se consiguen las gemas totales
     internal float GetGemasTotales()
     {
         return gemasTotales;
     }
+    #endregion
 }
